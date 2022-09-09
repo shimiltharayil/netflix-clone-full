4fa18ef42e2c2ed27899b71ff7f53bfe7b2e02f1
Shimil Tharayil <shimiltharayil007@gmail.com>
2022-09-09 20:54:27 +0530

    Row added

--- src/Components/HomeScreen.js
+++ src/Components/HomeScreen.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import requests from '../Requests';
 import Banner from './Banner';
 import Navbar from './Navbar';
+import Row from './Row';
 
 function HomeScreen() {
   return (
     <div className='homeScreen'>
         <Navbar />
         <Banner />
-       
+       <Row
+        title = "NETFLIX ORIGINALS"
+        fetchUrl = {requests.fetchNetflixOriginal}
+       isLargeRow = {true}
+         />
+         <Row title = "Trending Now" fetchUrl = { requests.fetchTrending} /> 
+         <Row title = "Action Movies" fetchUrl = {requests.fetchActionMovies } /> 
+         <Row title = "Comedy Movies" fetchUrl = { requests.fetchComedyMovies} /> 
+         <Row title = "Horror Movies" fetchUrl = {requests.fetchHorrorMovies } /> 
+         <Row title = "Documentaries" fetchUrl = { requests.fetchDocumentaries} /> 
+         <Row title = "Anime" fetchUrl = {requests.fetchAnime } /> 
+      
     </div>
   )
 }
--- src/Requests.js
+++ src/Requests.js
@@ -3,11 +3,11 @@ const API_KEY = "367418dc6317f42645c39965eb1a6680";
 
 const requests = {
     fetchTrending : `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-   fetchNetflixOriginal : `discover/tv?api_key=${API_KEY}&with_networks=213`,
- fetchActionMovies : `discover/tv?api_key=${API_KEY}&with_networks=28`,
- fetchComedyMovies :  `discover/tv?api_key=${API_KEY}&with_networks=35`,
- fetchHorrorMovies : `discover/tv?api_key=${API_KEY}&with_networks=25`,
-fetchDocumentries :  `discover/tv?api_key=${API_KEY}&with_networks=99`,
+   fetchNetflixOriginal : `discover/tv?api_key=${API_KEY}&with_networks=3`,
+   fetchActionMovies : `discover/tv?api_key=${API_KEY}&with_networks=28`,
+  fetchComedyMovies :  `discover/tv?api_key=${API_KEY}&with_networks=35`,
+  fetchHorrorMovies : `discover/tv?api_key=${API_KEY}&with_networks=25`,
+ fetchDocumentaries :  `discover/tv?api_key=${API_KEY}&with_networks=99`,
  fetchAnime: `discover/tv?api_key=${API_KEY}&with_networks=98`,
 };
 export default requests;
